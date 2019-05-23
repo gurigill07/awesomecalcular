ae25d4f8d2e7749d0dc66338026b215f5890b39a
gurigill07 <50706248+gurigill07@users.noreply.github.com>
2019-05-23 10:21:43 -0400

    Add files via upload
    addition of two no .

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AwesomeCalculator;
+using classtest1;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace classtest1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void GetAddition_Input3point4and5point6_Returns9point0()
+        {
+            //Arrange
+            double number1 = 3.4;
+            double number2 = 5.6;
+            double expectedResult = number1 + number2;
+            Calc testCalc = new Calc(number1, number2);
+            //Act
+            double actualResult = testCalc.GetAddition();
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
